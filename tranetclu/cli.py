7c530eadbb44c901b33576bc18df80169a3b0aaf
KarstenEconomou <economoukarsten@gmail.com>
2024-01-19 16:57:14 -0400

    Finalized basic functionality

--- tranetclu/cli.py
+++ tranetclu/cli.py
@@ -3,20 +3,20 @@ import click
 from tranetclu.networkops import NetworkOps
 
 @click.command()
-@click.argument("file", type=str, required=True)
-@click.option("--outfile", ype=str, required=True, help="File to write network to")
-@click.option("--resolution", "-res", default=5, type=int, required=False, help="H3 grid resolution (0-15)")
-def make(in_file: str, out_file: str, res: int):
+@click.argument("in-file", type=click.Path(exists=True), required=True)
+@click.option("--out-file", "-o", type=click.Path(), required=True, help="File to write network to")
+@click.option("--resolution", "-res", "res", default=5, type=int, required=False, help="H3 grid resolution (0-15)")
+def make_net(in_file: click.Path, out_file: click.Path, res: int):
     click.echo("Hello makenet!")
     net = NetworkOps.from_locations(in_file, res)
     net.to_file(out_file)
 
 @click.command()
-@click.argument("file", type=str, required=True)
-@click.option("--outfile", ype=str, required=True, help="File to write partition to")
-@click.option("--num_trials", default=20, type=int, required=False, help="Number of outer trials to perform")
-@click.option("--markov_time", default=1, type=float, required=False, help="Markov time tuning parameter")
-@click.option("--seed", default=42, type=int, required=False, help="Random seed")
-def partition(file: str, num_trials: int, markov_time: float, seed: int):
-    net = NetworkOps.from_file(file)
-    net.partition(num_trials, markov_time, seed)
+@click.argument("in-file", type=click.Path(exists=True), required=True)
+@click.option("--out-file", "-o", type=click.Path(), required=True, help="File to write partition to")
+@click.option("--num-trials", "-n", "num_trials", default=20, type=int, required=False, help="Number of outer trials to perform")
+@click.option("--markov-time", "-mt", "markov_time", default=1, type=float, required=False, help="Markov time tuning parameter")
+@click.option("--seed", "-s", "seed", default=42, type=int, required=False, help="Random seed")
+def partition(in_file: click.Path, out_file: click.Path, num_trials: int, markov_time: float, seed: int):
+    net = NetworkOps.from_file(in_file)
+    net.partition(out_file, num_trials, markov_time, seed)
--- tranetclu/networkops.py
+++ tranetclu/networkops.py
@@ -14,12 +14,18 @@ type Partition = tuple[set[Node], ...]
 class NetworkOps:
     network: nx.DiGraph
 
-    def to_file(self, path: str) -> None:
-        nx.write_edgelist(self.network, path, delimiter=",", comments="#")
+    def to_file(self, path: click.Path) -> None:
+        nx.write_edgelist(
+            self.network,
+            path,
+            delimiter=",",
+            comments="#",
+            data=['wgt', 'wgt_nrm'],
+        )
 
     def partition(
             self,
-            file: str,
+            path: click.Path,
             num_trials: int,
             markov_time: float,
             seed: int,
@@ -32,36 +38,36 @@ class NetworkOps:
             markov_time=markov_time,
             seed=seed,
         )
-        _ = im.add_networkx_graph(self.network)
+        _ = im.add_networkx_graph(self.network, weight="wgt")
         im.run()
 
         partition = im.get_dataframe(["name", "module_id", "flow", "modular_centrality"])
-        partition["name", "module_id", "flow"].to_csv(file)
+        partition[["name", "module_id", "flow"]].to_csv(path, index=False, header=False)
         #tuple(partition.groupby("module_id")["name"].apply(set))
 
     @classmethod
-    def from_locations(cls, path: str, res: int) -> typing.Self:
+    def from_locations(cls, path: click.Path, res: int) -> typing.Self:
         data = pd.read_csv(
             path,
-            usecols=["initial_lng", "initial_lat", "final_lng", "final_lat"],
+            names=["initial_lng", "initial_lat", "final_lng", "final_lat"],
             index_col=False,
             comment="#",
         )
 
+        click.echo(f"Binning {data.shape[0]} particle positions")
         srcs = cls.bin_positions(data["initial_lng"], data["initial_lat"], res)
         tgts = cls.bin_positions(data["final_lng"], data["final_lat"], res)
         edges = tuple(zip(srcs, tgts))
-
         return NetworkOps(cls.construct_net(edges))
     
     @classmethod
-    def from_file(cls, path: str) -> typing.Self:
+    def from_file(cls, path: click.Path) -> typing.Self:
         net = nx.read_edgelist(
-            path, 
-            comments="#", 
+            path,
+            comments="#",
             delimiter=",",
             create_using=nx.DiGraph,
-            nodetype=int,
+            nodetype=str,
             data=[("wgt", float), ("wgt_nrm", float)],
         )
         return NetworkOps(net)
@@ -69,7 +75,6 @@ class NetworkOps:
     @staticmethod
     def bin_positions(lngs: typing.Sequence[float], lats: typing.Sequence[float], res: int) -> list[Node]:
         bins = [h3.latlng_to_cell(lat, lng, res) for lat, lng in zip(lats, lngs)]
-        click.echo(f"Binned {len(lngs)} particle trajectories into {len(set(bins))} bins")
         return bins
     
     @staticmethod
@@ -84,7 +89,7 @@ class NetworkOps:
                 net.add_edge(src, tgt, wgt=1)
 
         for node in net.nodes:
-            out_weight = sum(net.out_edges(node, data='wgt', default=0).values())
+            out_weight = sum(wgt for _, _, wgt in net.out_edges(node, data='wgt', default=0))
             for neighbour in net.successors(node):
                 net[node][neighbour]["wgt_nrm"] = net[node][neighbour]["wgt"] / out_weight if out_weight != 0 else 0
 
