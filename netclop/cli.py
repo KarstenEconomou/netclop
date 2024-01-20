d0b16739aaea2668b60b9bb41065ecca24a55da4
KarstenEconomou <economoukarsten@gmail.com>
2024-01-20 13:29:45 -0400

    Major cleaning and streamline

--- netclop/cli.py
+++ netclop/cli.py
@@ -3,7 +3,11 @@ import click
 from netclop.networkops import NetworkOps
 from netclop.plot import Plot
 
-@click.command()
+@click.group()
+def netclop():
+    pass
+
+@netclop.command()
 @click.argument("in-file",
     type=click.Path(exists=True),
     required=True
@@ -12,20 +16,22 @@ from netclop.plot import Plot
     type=click.Path(),
     default=None,
     required=False,
-    help="File to write network to"
+    help="File to write network to."
 )
-@click.option("--resolution", "-res", "res",
+@click.option("--resolution", "-res",
     type=int,
     default=5,
+    show_default=True,
     required=False,
-    help="H3 grid resolution (0-15)"
+    help="H3 grid resolution (0-15) for domain discretization."
 )
-def construct_net(in_file: click.Path, out_file: click.Path, res: int):
-    net = NetworkOps.from_locations(in_file, res)
+def construct_net(in_file: click.Path, out_file: click.Path, resolution: int) -> None:
+    """Constructs a network from particle positions."""
+    net = NetworkOps.from_locations(in_file, resolution)
     if out_file is not None:
         net.to_file(out_file)
 
-@click.command()
+@netclop.command()
 @click.argument("in-file",
     type=click.Path(exists=True),
     required=True
@@ -34,72 +40,48 @@ def construct_net(in_file: click.Path, out_file: click.Path, res: int):
     type=click.Path(),
     default=None,
     required=False,
-    help="File to write partition to",
+    help="File to write partition to.",
 )
-@click.option("--num-trials", "-n", "num_trials",
+@click.option("--num-trials", "-n",
+    type=int,
     default=20,
-    type=int,
+    show_default=True,
     required=False,
-    help="Number of outer trials to perform",
+    help="Number of outer trials to perform.",
 )
-@click.option("--markov-time", "-mt", "markov_time",
-    default=1,
+@click.option("--markov-time", "-mt",
     type=float,
+    default=1,
+    show_default=True,
     required=False,
-    help="Markov time spatial scale tuning parameter",
+    help="Markov time spatial scale tuning parameter.",
 )
-@click.option("--seed", "-s", "seed",
-    default=42,
+@click.option("--seed", "-s",
     type=int,
+    default=42,
+    show_default=True,
     required=False,
-    help="Random seed",
+    help="Random seed.",
 )
-def partition(in_file: click.Path, out_file: click.Path, num_trials: int, markov_time: float, seed: int):
+def partition(
+    in_file: click.Path,
+    out_file: click.Path,
+    num_trials: int,
+    markov_time: float,
+    seed: int,
+) -> None:
+    """Partitions a network."""
     net = NetworkOps.from_file(in_file)
     net.partition(num_trials, markov_time, seed)
     if out_file is not None:
         net.partition_to_file(out_file)
 
-@click.command()
+@netclop.command()
 @click.argument("in-file",
     type=click.Path(exists=True),
     required=True,
 )
-def plot(in_file: click.Path):
+def plot(in_file: click.Path) -> None:
+    """Plots a network partition."""
     plt = Plot.from_file(in_file)
-    plt.plot()
-
-@click.command()
-@click.argument("in-file",
-    type=click.Path(exists=True),
-    required=True
-)
-@click.option("--resolution", "-res", "res",
-    type=int,
-    default=5,
-    required=False,
-    help="H3 grid resolution (0-15)"
-)
-@click.option("--num-trials", "-n", "num_trials",
-    default=20,
-    type=int,
-    required=False,
-    help="Number of outer trials to perform",
-)
-@click.option("--markov-time", "-mt", "markov_time",
-    default=1,
-    type=float,
-    required=False,
-    help="Markov time spatial scale tuning parameter",
-)
-@click.option("--seed", "-s", "seed",
-    default=42,
-    type=int,
-    required=False,
-    help="Random seed",
-)
-def quick_plot(in_file: click.Path, res: int, num_trials: int, markov_time: float, seed: int) -> None:
-    net = NetworkOps.from_locations(in_file, res)
-    net.partition(num_trials, markov_time, seed)
-    plt = Plot.from_df(net.modular)
     plt.plot()
